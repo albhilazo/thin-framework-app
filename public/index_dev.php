feff22e0099d8a09a0ba3ae68fac9b7cd60f1766
Albert Hilazo <albhilazo@gmail.com>
2016-07-24 22:02:25 +0200

    Add development entry point index_dev.php

---
+++ public/index_dev.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use ThinFramework\Component\Bootstrap\Bootstrap;
+
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+
+new Bootstrap();
