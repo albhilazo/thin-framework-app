e1cc1203b4e59132b47fb52094ad10ec51af27f8
Albert Hilazo <albhilazo@gmail.com>
2016-07-31 23:36:27 +0200

    Update production front controller

--- public/index.php
+++ public/index.php
@@ -3,6 +3,17 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 use ThinFramework\Component\Bootstrap\Bootstrap;
+use ThinFramework\Component\Request\Request;
 
+$config = [
+    'routing_path'      => __DIR__.'/../app/routing/routing.yml',
+    // 'templating_path'   => __DIR__.'/../src/View/smarty',
+    // 'templating_engine' => 'Smarty',
+    'templating_path'   => __DIR__.'/../src/View/twig',
+    'templating_engine' => 'Twig',
+    'services_path'     => __DIR__.'/../app/services/services_prod.yml',
+];
 
-new Bootstrap();
+$app     = new Bootstrap($config);
+$request = new Request();
+$app($request);
