730cca006a2a600eff2048e99b9e4462c26734a6
Albert Hilazo <albhilazo@gmail.com>
2016-07-31 21:08:47 +0200

    Update controllers to handle Request object

--- src/Controller/NotFound404Controller.php
+++ src/Controller/NotFound404Controller.php
@@ -4,6 +4,7 @@ namespace ThinApp\Controller;
 
 use ThinFramework\Component\Controller\ThinController;
 use ThinFramework\Component\Templating\TemplatingAdapter;
+use ThinFramework\Component\Request\Request;
 
 
 class NotFound404Controller extends ThinController
@@ -12,9 +13,9 @@ class NotFound404Controller extends ThinController
     const TEMPLATE = 'example.tpl';
 
 
-    public function __construct(TemplatingAdapter $templating)
+    public function __construct(TemplatingAdapter $templating, Request $request)
     {
-        parent::__construct($templating);
+        parent::__construct($templating, $request);
 
         $this->template->setLayout(self::TEMPLATE);
         $this->template->setParam('head_title', 'Error 404');
