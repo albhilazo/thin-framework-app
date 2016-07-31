7be581b6d38c599a750470122aaf63f037963704
Albert Hilazo <albhilazo@gmail.com>
2016-07-31 23:42:17 +0200

    Update controllers to handle Container

--- src/Controller/NotFound404Controller.php
+++ src/Controller/NotFound404Controller.php
@@ -3,6 +3,7 @@
 namespace ThinApp\Controller;
 
 use ThinFramework\Component\Controller\ThinController;
+use ThinFramework\Component\Container\Container;
 use ThinFramework\Component\Templating\TemplatingAdapter;
 use ThinFramework\Component\Request\Request;
 
@@ -13,9 +14,9 @@ class NotFound404Controller extends ThinController
     const TEMPLATE = 'example.tpl';
 
 
-    public function __construct(TemplatingAdapter $templating, Request $request)
+    public function __construct(Container $container, TemplatingAdapter $templating, Request $request)
     {
-        parent::__construct($templating, $request);
+        parent::__construct($container, $templating, $request);
 
         $this->template->setLayout(self::TEMPLATE);
         $this->template->setParam('head_title', 'Error 404');
